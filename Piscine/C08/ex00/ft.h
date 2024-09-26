8d91b7602d63a0101e27413e4bfef43805deaecd
Noe Rabinovici <norabino@c3r4p8.42perpignan.fr>
2024-09-26 17:43:44 +0200

    cest moi le caca skibidi tooolet quarante deux mecs pour mon cul

---
+++ Piscine/C08/ex00/ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: norabino <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/18 09:45:59 by norabino          #+#    #+#             */
+/*   Updated: 2024/09/18 10:08:51 by norabino         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+
+#endif
---
+++ Piscine/C08/ex04/ft_strs_to_tab.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strs_to_tab.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: norabino <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/26 09:53:03 by norabino          #+#    #+#             */
+/*   Updated: 2024/09/26 09:59:52 by norabino         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_STRS_TO_TAB_H
+# define FT_STRS_TO_TAB_H
+
+typedef struct s_stock_str
+{
+	int		size;
+	char	*str;
+	char	*copy;
+}			t_stock_str;
+
+#endif
